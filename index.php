e29836632bbc549e25d31873fed6b88da8c33d16
Abraham00000 <chelasantoya03@gmail.com>
2021-05-26 12:47:58 -0500

    subiendo los primeros archivos

---
+++ index.php
@@ -0,0 +1,132 @@
+
+<h1 style="text-align: center;">Donde la reina es del equipo blanco y sus rivales son las fichas de color negro</h1>
+
+<p>oprimiendo el valor del boton podemos saber en todas las direciones en las que puede atacar</p>
+
+<table width="400" height="400px"; border="0" cellspacing="2" cellpadding="2" bgcolor="#000000">
+<tr align="center">
+<td><font id="demo">1</font></td>
+<td bgcolor="#ffffff"><img src="img/pieza.png" width="30"></td>
+<td><font color="#ffffff" id="demo2">3</font></td>
+<td bgcolor="#ffffff">4</td>
+<td><font color="#ffffff">5</font></td>
+<td bgcolor="#ffffff">6</td>
+<td><font color="#ffffff">7</font></td>
+<td bgcolor="#ffffff">8</td>
+</tr>
+<tr align="center">
+<td bgcolor="#ffffff">9</td>
+<td><font color="#ffffff">10</font></td>
+<td bgcolor="#ffffff">11</td>
+<td><font color="#ffffff">12</font></td>
+<td bgcolor="#ffffff">13</td>
+<td><font color="#ffffff">14</font></td>
+<td bgcolor="#ffffff">15</td>
+<td><font color="#ffffff">16</font></td>
+</tr>
+<tr align="center">
+<td><font color="#ffffff">17</font></td>
+<td bgcolor="#ffffff">18</td>
+<td><font color="#ffffff">19</font></td>
+<td bgcolor="#ffffff">20</td>
+<td><font color="#ffffff">21</font></td>
+<td bgcolor="#ffffff">22</td>
+<td><font color="#ffffff">23</font></td>
+<td bgcolor="#ffffff">24</td>
+</tr>
+<tr align="center">
+<td bgcolor="#ffffff">25</td>
+<td><font color="#ffffff" id="demo13">26</font></td>
+<td bgcolor="#ffffff">27</td>
+<td><font color="#ffffff" id="demo14">28</font></td>
+<td bgcolor="#ffffff">29</td>
+<td><font color="#ffffff" id="demo15">30</font></td>
+<td bgcolor="#ffffff">31</td>
+<td><font color="#ffffff" id="demo16">32</font></td>
+</tr>  <tr align="center">
+<td><font color="#ffffff" id="demo17">33</font></td>
+<td bgcolor="#ffffff">34</td>
+<td><font color="#ffffff" id="demo18">35</font></td>
+<td bgcolor="#ffffff">36</td>
+<td><font color="#ffffff" id="demo19">37</font></td>
+<td bgcolor="#ffffff">38</td>
+<td><font color="#ffffff" id="demo20">39</font></td>
+<td bgcolor="#ffffff">40</td>
+</tr>
+<tr align="center">
+<td bgcolor="#ffffff">41</td>
+<td><font color="#ffffff" id="demo21">42</font></td>
+<td bgcolor="#ffffff">43</td>
+<td><font color="#ffffff" id="demo22">44</font></td>
+<td bgcolor="#ffffff">45</td>
+<td><font color="#ffffff" id="demo23">46</font></td>
+<td bgcolor="#ffffff">47</td>
+<td><font color="#ffffff" id="demo24">48</font></td>
+</tr>  <tr align="center">
+<td><font color="#ffffff" id="demo25">49</font></td>
+<td bgcolor="#ffffff" >50</td>
+<td><font color="#ffffff" id="demo26">51</font></td>
+<td bgcolor="#ffffff" >52</td>
+<td><font color="#ffffff" id="demo27">53</font></td>
+<td bgcolor="#ffffff" >54</td>
+<td><font color="#ffffff" id="demo28">55</font></td>
+<td bgcolor="#ffffff" >56</td>
+</tr>  <tr align="center">
+<td bgcolor="#ffffff">57</td>
+<td><font color="#ffffff" id="demo29">58</font></td>
+<td bgcolor="#ffffff">59</td>
+<td><font color="#ffffff" id="demo30">60</font></td>
+<td bgcolor="#ffffff">61</td>
+<td><font color="#ffffff" id="demo31">62</font></td>
+<td bgcolor="#ffffff">63</td>
+<td><font color="#ffffff" id="demo32">64</font></td>
+</tr>
+</table>
+
+
+
+
+
+	<button onclick="reinaataca()">Boton 1</button>
+
+
+<script>
+function reinaataca() {
+   document.getElementById("demo13").innerHTML = "Posicion Atacar";
+   document.getElementById("demo14").innerHTML = "Posicion Atacar";
+   document.getElementById("demo14").innerHTML = "Posicion Atacar";
+   document.getElementById("demo15").innerHTML = "Posicion Atacar";
+   document.getElementById("demo16").innerHTML = "Posicion Atacar";
+   document.getElementById("demo17").innerHTML = "Posicion Atacar";
+   document.getElementById("demo18").innerHTML = "Posicion Atacar";
+   document.getElementById("demo19").innerHTML = "Posicion Atacar";
+   document.getElementById("demo20").innerHTML = "Posicion Atacar";
+   document.getElementById("demo21").innerHTML = "Posicion Atacar";
+   document.getElementById("demo22").innerHTML = "Posicion Atacar";
+   document.getElementById("demo23").innerHTML = "Posicion Atacar";
+   document.getElementById("demo24").innerHTML = "Posicion Atacar";
+   document.getElementById("demo25").innerHTML = "Posicion Atacar";
+   document.getElementById("demo26").innerHTML = "Posicion Atacar";
+   document.getElementById("demo27").innerHTML = "Posicion Atacar";
+   document.getElementById("demo28").innerHTML = "Posicion Atacar";
+   document.getElementById("demo29").innerHTML = "Posicion Atacar";
+   document.getElementById("demo30").innerHTML = "Posicion Atacar";
+   document.getElementById("demo31").innerHTML = "Posicion Atacar";
+   document.getElementById("demo32").innerHTML = "Posicion Atacar";
+   document.getElementById("demo33").innerHTML = "Posicion Atacar";
+
+}
+</script>
+
+
+
+
+
+<?php
+
+if (isset($_REQUEST['ataca'])) {
+	
+}
+
+
+?>
